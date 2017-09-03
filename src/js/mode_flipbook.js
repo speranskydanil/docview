e0674972403d55baaa520aae1b86d86b725a097a
Danil Speransky <speranskydanil@gmail.com>
2017-09-03 18:23:40 +0300

    refactoring

--- src/js/mode_flipbook.js
+++ src/js/mode_flipbook.js
@@ -9,17 +9,18 @@ export default class ModeFlipBook extends Mode {
   activate(index, zoom, scroll) {
     super.activate(index, zoom)
 
+    this.showPages()
+
+    this.dom.pages.css('position', 'absolute')
+
     for (let [i, page] of this.pages.entries()) {
       if (i % 2 == 0) {
-        page.div.css({ right: 0, top: 0 }).click(() => this.next())
+        page.div.css({right: 0, top: 0}).click(() => this.next())
       } else {
         page.div.css({left: 0, top: 0}).click(() => this.prev())
       }
     }
 
-    this.dom.pages.css('position', 'absolute')
-
-    this.showPages()
     this.redraw()
 
     if (scroll) this.scroll()
@@ -27,11 +28,10 @@ export default class ModeFlipBook extends Mode {
 
   deactivate() {
     super.deactivate()
-
-    this.dom.pages.unbind('click').css('position', 'relative').show()
-
-    this.dom.wrapper.css({ width: '100%', height: 'auto' })
-
+    this.dom.pages.show()
+    this.dom.pages.css('position', 'relative')
+    this.dom.pages.off('click')
+    this.dom.wrapper.css({width: '100%', height: 'auto'})
     this.dom.viewport.top_scrollbar(false)
   }
 
@@ -49,11 +49,10 @@ export default class ModeFlipBook extends Mode {
 
   next() {
     if (this.animation) return
-    if (this.index + (this.index % 2) - 1 + 2 > this.pages.length - 1) return
+    if (this.realIndex > this.pages.length - 3) return
 
     this.animation = true
-
-    let index = this.index + (this.index % 2) - 1
+    let index = this.realIndex
 
     let p1 = this.pages[index]
     let p2 = this.pages[index + 1]
@@ -63,23 +62,22 @@ export default class ModeFlipBook extends Mode {
     if (p4) p4.div.css('z-index', -1).show()
 
     p2.img.height(this.zooms[this.zoom] * p2.ratio)
-    p2.img.animate({ width: 0 }, 180, () => {
+    p2.img.animate({width: 0}, 180, () => {
       p2.div.hide()
 
-      p3.img.css({ width: 0, right: 0 })
+      p3.img.css({width: 0, right: 0})
       p3.div.show()
 
       p3.img.height(this.zooms[this.zoom] * p3.ratio)
-      p3.img.animate({ width: this.pageWidth }, 180, () => {
+      p3.img.animate({width: this.pageWidth}, 180, () => {
         if (p1) p1.div.hide()
-
         if (p4) p4.div.css('z-index', 'auto')
+
         p2.img.removeAttr('style')
         p3.img.removeAttr('style')
 
         this.changeIndex(this.index + 2)
         $(window).trigger('dv_change')
-
         this.animation = false
       })
     })
@@ -87,11 +85,10 @@ export default class ModeFlipBook extends Mode {
 
   prev() {
     if (this.animation) return
-    if (this.index + (this.index % 2) - 1 - 1 < 0) return
+    if (this.realIndex < 1) return
 
     this.animation = true
-
-    let index = this.index + (this.index % 2) - 1
+    let index = this.realIndex
 
     let p1 = this.pages[index - 2]
     let p2 = this.pages[index - 1]
@@ -103,14 +100,14 @@ export default class ModeFlipBook extends Mode {
     p3.img.css('right', 0)
 
     p3.img.height(this.zooms[this.zoom] * p3.ratio)
-    p3.img.animate({ width: 0 }, 180, () => {
+    p3.img.animate({width: 0}, 180, () => {
       p3.div.hide()
 
       p2.img.css('width', 0)
       p2.div.css('z-index', 1).show()
 
       p2.img.height(this.zooms[this.zoom] * p2.ratio)
-      p2.img.animate({ width: this.pageWidth }, 180, () => {
+      p2.img.animate({width: this.pageWidth}, 180, () => {
         if (p4) p4.div.hide()
 
         p3.img.removeAttr('style')
@@ -119,36 +116,30 @@ export default class ModeFlipBook extends Mode {
 
         this.changeIndex(this.index - 2)
         $(window).trigger('dv_change')
-
         this.animation = false
       })
     })
   }
 
   changeIndex(index) {
-    if (index < 0) index = 0
-    if (index > this.pages.length - 1) index = this.pages.length - 1
-
-    if (this.index != index) {
-      this.index = index
-      this.showPages()
-
-      this.queue.clear()
-      this.load()
-    }
+    this.index = Math.min(Math.max(index, 0), this.pages.length - 1)
+    this.showPages()
+    this.queue.clear()
+    this.load()
   }
 
   showPages() {
     this.dom.pages.hide()
 
-    if (this.index == 0) {
-      this.pages[this.index].div.show()
-    } else {
-      let index = this.index + (this.index % 2) - 1
-      this.pages[index].div.show()
+    if (this.index == 0)
+      return this.page.div.show()
 
-      if (index + 1 < this.pages.length) this.pages[index + 1].div.show()
-    }
+    this.pages[this.realIndex].div.show()
+    if (this.realIndex + 1 < this.pages.length) this.pages[this.realIndex + 1].div.show()
+  }
+
+  get realIndex() {
+    return this.index + (this.index % 2) - 1
   }
 
   redraw() {
@@ -159,30 +150,25 @@ export default class ModeFlipBook extends Mode {
 
   resize() {
     this.resizePages()
-
-    this.dom.wrapper.css({
-      width: 2 * this.pageWidth,
-      height: this.pageHeight
-    })
-
+    this.dom.wrapper.css({width: 2 * this.pageWidth, height: this.pageHeight})
     this.dom.viewport.top_scrollbar()
   }
 
   load() {
-    let index = Math.max(0, this.index + (this.index % 2) - 1)
+    let index = Math.max(0, this.realIndex)
 
     this.queue.add(this.pages[index], this.zoom)
     if (index + 1 < this.pages.length) this.queue.add(this.pages[index + 1], this.zoom)
 
-    let numberOfImagesOnRight = Math.min(8, this.pages.length - index - 2)
+    let rightImages = Math.min(5, this.pages.length - index - 2)
 
-    for (let i = 1; i <= numberOfImagesOnRight; i += 1) {
-      this.queue.add(this.pages[(index + 1) + i], this.zoom)
+    for (let i = 1; i <= rightImages; i += 1) {
+      this.queue.add(this.pages[index + 1 + i], this.zoom)
     }
 
-    let numberOfImagesOnLeft = Math.min(8, index)
+    let leftImages = Math.min(5, index)
 
-    for (let i = 1; i <= numberOfImagesOnLeft; i += 1) {
+    for (let i = 1; i <= leftImages; i += 1) {
       this.queue.add(this.pages[index - i], this.zoom)
     }
   }
