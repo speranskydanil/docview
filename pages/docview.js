787bab5925c95853c6a7c580e81f9dba708a3494
Danil Speransky <speranskydanil@gmail.com>
2017-09-05 10:30:43 +0300

    fix readme

--- pages/docview.js
+++ pages/docview.js
@@ -1,7 +1,7 @@
 var docview = new Docview({
   div: '.docview',
   zooms: [82, 164, 328, 492, 656, 818, 984, 1146],
-  pages: [...Array(20)].map(i => ({id: i + 1, w: 1146, h: 1699})),
+  pages: [...Array(20)].map(_, i) => ({id: i + 1, w: 1146, h: 1699})),
   pageUrl: function(id, zoom) {
     return ['/docview/pages', id, zoom, id + '.jpg'].join('/');
   }
